d65fe8314f53cc9d55abb9987df95ebeaf52d35b
tongnhatminhvietnam <155602912+tongnhatminhvietnam@users.noreply.github.com>
2024-01-04 23:34:02 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
+// Hàm xử lý sự kiện click của button "Nhận mã số may mắn"
 function checkPhoneNumber() {
-  var phoneNumber = document.getElementById("phone_number").value;
-
   // Gọi hàm checkPhoneNumber() trong file Java
   var luckyNumber = Java.call("main.CheckPhoneNumber", phoneNumber);
 
   // Hiển thị mã số may mắn
   document.getElementById("lucky_number").innerHTML = luckyNumber;
-}
 
-// Gắn sự kiện click cho button "Nhận mã số may mắn"
-document.getElementById("submit").onclick = checkPhoneNumber;
-
-// Hàm hiển thị popup
-function showPopup() {
+  // Hiển thị layer 2
   document.querySelector(".popup").style.display = "block";
-  document.querySelector(".popup").style.zIndex = 2;
 }
 
 // Hàm xử lý sự kiện click của button "đóng"
 function closePopup() {
   // Ẩn layer 2
   document.querySelector(".popup").style.display = "none";
+
+  // Hiển thị layer 1
+  document.querySelector(".container").style.display = "block";
 }
 
-// Gắn sự kiện click cho button "đóng"
-document.getElementById("close").onclick = closePopup;
-
+// Gắn sự kiện click cho button "Nhận mã số may mắn"
+document.getElementById("submit").onclick = checkPhoneNumber;
