11c7e721980a9f4f0b5a65c152223b25840b2812
tongnhatminhvietnam <155602912+tongnhatminhvietnam@users.noreply.github.com>
2024-01-04 23:28:15 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,27 +1,33 @@
 function checkPhoneNumber() {
   var phoneNumber = document.getElementById("phone_number").value;
 
-  // Kiểm tra số điện thoại và hiển thị mã số may mắn tương ứng
-  if (phoneNumber == "01234567891") {
-    document.getElementById("lucky_number").innerHTML = "1";
-  } else if (phoneNumber == "0123456789") {
-    document.getElementById("lucky_number").innerHTML = "2";
-  } else {
-    // Hiển thị popup với thông báo hỗ trợ
-    showPopup();
-    document.getElementById("lucky_number").innerHTML = "Xin lỗi bạn vui lòng liên hệ anh Nhật Minh sđt 08588813988 để được hỗ trợ";
-  }
+  // Gọi hàm checkPhoneNumber() trong file Java
+  var luckyNumber = Java.call("main.CheckPhoneNumber", phoneNumber);
+
+  // Hiển thị mã số may mắn
+  document.getElementById("lucky_number").innerHTML = luckyNumber;
 }
 
 // Gắn sự kiện click cho button "Nhận mã số may mắn"
-document.getElementById("submit").onclick = function() {
-  checkPhoneNumber();
-};
+document.getElementById("submit").onclick = checkPhoneNumber;
 
 // Hàm hiển thị popup
 function showPopup() {
   document.querySelector(".popup").style.display = "block";
   document.querySelector(".popup").style.zIndex = 2;
 }
+
+// Hàm xử lý sự kiện click của button "đóng"
+function closePopup() {
+  // Ẩn layer 2
+  document.querySelector(".popup").style.display = "none";
+
+  // Hiển thị layer 1
+  document.querySelector(".container").style.display = "block";
+}
+
 // Đặt mặc định là hiển thị layer 1
 document.querySelector(".container").style.display = "block";
+
+// Chỉnh màu chữ thành màu đen ở trong input text
+document.querySelector("#phone_number").style.color = "black";
