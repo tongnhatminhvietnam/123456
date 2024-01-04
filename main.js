42b69f033d16e875c9589b5c2d0ebebeb64144ac
tongnhatminhvietnam <155602912+tongnhatminhvietnam@users.noreply.github.com>
2024-01-04 22:35:33 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+function checkPhoneNumber() {
+  var phoneNumber = document.getElementById("phone_number").value;
+  if (phoneNumber == "01234567891") {
+    document.getElementById("lucky_number").innerHTML = "1";
+  } else if (phoneNumber == "0123456789") {
+    document.getElementById("lucky_number").innerHTML = "2";
+  } else {
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("lucky_number").innerHTML = "Xin lỗi bạn vui lòng liên hệ anh Nhật Minh sđt 08588813988 để được hỗ trợ";
+  }
+}
+
+document.getElementById("submit").onclick = function() {
+  checkPhoneNumber();
+};
