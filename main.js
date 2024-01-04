28e420db46d9611b45d5235c8e1e4b2ae4e8ef8b
tongnhatminhvietnam <155602912+tongnhatminhvietnam@users.noreply.github.com>
2024-01-04 23:51:21 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 // Hàm xử lý sự kiện click của button "Nhận mã số may mắn"
 function checkPhoneNumber() {
-  // Gọi hàm checkPhoneNumber() trong file Java
-  var phoneNumber = document.getElementById("phone_number").value;
-  var luckyNumber = Java.call("main.CheckPhoneNumber", phoneNumber);
+ // Gọi hàm checkPhoneNumber() trong file Java
+ var phoneNumber = document.getElementById("phone_number").value;
+ var luckyNumber = Java.call("main.CheckPhoneNumber", phoneNumber);
 
-  // Hiển thị mã số may mắn
-  document.getElementById("lucky_number").innerHTML = luckyNumber;
+ // Hiển thị mã số may mắn
+ document.getElementById("lucky_number").innerHTML = luckyNumber;
 
-  // Hiển thị layer 2
-  if (phoneNumber !== "") {
-    document.querySelector(".popup").style.display = "block";
-  }
+ // Hiển thị layer 2
+ document.querySelector(".popup").style.display = "block";
 
-  // Kiểm tra mã số may mắn
-  if (phoneNumber.match(/^01234567891$/) || phoneNumber.match(/^0123456789$/)) {
-    document.getElementById("lucky_number").innerHTML = "Mã số may mắn của bạn là " + luckyNumber;
-  } else {
-    document.getElementById("lucky_number").innerHTML = "Xin lỗi bạn không có trong danh sách khách mời, liên hệ ông Tống Nhật Minh 0858881398 để làm thủ tục check in";
-  }
+ // Kiểm tra mã số may mắn
+ if (phoneNumber.match(/^01234567891$/) || phoneNumber.match(/^0123456789$/)) {
+  document.getElementById("lucky_number").innerHTML = "Mã số may mắn của bạn là " + luckyNumber;
+ } else {
+  document.getElementById("lucky_number").innerHTML = "Xin lỗi bạn không có trong danh sách khách mời, liên hệ ông Tống Nhật Minh 0858881398 để làm thủ tục check in";
+ }
 
-  // Thêm thông tin về thời gian check-in
-  document.getElementById("time_check_in").innerHTML = "Thời gian check-in: 8h30 - 9h30";
+ // Thêm thông tin về thời gian check-in
+ document.getElementById("time_check_in").innerHTML = "Thời gian check-in: 8h30 - 9h30";
 }
